6272d913899694da4d50cd2277b851fdf503a5a7
ArJSarmiento <rneljan@gmail.com>
2024-04-14 12:02:42 +0800

    feat: refactored email sqs send implementation

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import os
 
 import boto3
 
-from model.email import EmailIn
+from model.email.email import EmailIn
 from usecase.email_usecase import EmailUsecase
 from utils.logger import logger
 
@@ -18,6 +18,9 @@ def send_email_handler(event, context):
     logger.info(records)
     for record in event['Records']:
         message_body = json.loads(record['body'])
-        email = EmailIn(**message_body)
-        email_usecase.send_email(email)
+
+        emails = [EmailIn(**message) for message in message_body]
+        for email in emails:
+            email_usecase.send_email(email)
+
         SQS.delete_message(QueueUrl=EMAIL_QUEUE, ReceiptHandle=record['receiptHandle'])
--- model/email/email.py
+++ model/email/email.py
@@ -1,12 +1,31 @@
+from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, Extra
+from model.entities import Entities
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute
 
 from constants.common_constants import EmailType
 from template.get_template import html_template
 
+class EmailTracker(Entities, discriminator='EmailTracker'):
+    # hk: EmailTracker
+    # rk: v<version_number>
+    lastEmailSent = UnicodeAttribute(null=True)
+    dailyEmailCount = NumberAttribute(null=True)
+
+class EmailTrackerIn(BaseModel):
+    class Config:
+        extra = Extra.ignore
+    
+    lastEmailSent: Optional[datetime] = Field(None, title='Last email sent')
+    dailyEmailCount: Optional[int] = Field(None, title='Daily email count')
+
 
 class EmailIn(BaseModel):
+    class Config:
+        extra = Extra.ignore
+
     to: Optional[List[EmailStr]] = Field(None, title='Email address of the recipient')
     cc: Optional[List[EmailStr]] = Field(None, title='CC Email addresses')
     bcc: Optional[List[EmailStr]] = Field(None, title='BCC Email address')
@@ -16,5 +35,4 @@ class EmailIn(BaseModel):
     regards: List[str] = Field(..., title='Regards of the email')
     emailType: EmailType = Field(..., title='Type of the email')
     eventId: str = Field(..., title='Event ID of the email')
-    useBackupSMTP: bool = Field(..., title='Use backup SMTP Service')
     content: str = Field(default=html_template())
