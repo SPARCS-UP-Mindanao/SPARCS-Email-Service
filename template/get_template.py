e32308e24924d867acd7bc9a973eadb02167f1bf
ArJSarmiento <rneljan@gmail.com>
2024-08-13 23:03:55 +0800

    feat: fix template

--- template/get_template.py
+++ template/get_template.py
@@ -1,6 +1,6 @@
 def html_template(is_sparcs=True):
     template_file = 'emailTemplate.html' if is_sparcs else 'nonSparcsEmailTemplate.html'
-    template_path = f'../template/{template_file}'
+    template_path = f'./template/{template_file}'
     with open(template_path, 'r') as template:
         content = template.read()
     return content
